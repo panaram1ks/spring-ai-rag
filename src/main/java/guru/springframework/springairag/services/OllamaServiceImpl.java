0d47435f401751fa6f977ded4703a585f91856de
parom <parom.evgen@mail.ru>
2026-03-01 11:53:01 +0300

    51. Configure System Prompt and Test

--- src/main/java/guru/springframework/springairag/services/OllamaServiceImpl.java
+++ src/main/java/guru/springframework/springairag/services/OllamaServiceImpl.java
@@ -3,6 +3,7 @@ package guru.springframework.springairag.services;
 import guru.springframework.springairag.model.Answer;
 import guru.springframework.springairag.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -27,27 +28,42 @@ public class OllamaServiceImpl implements OllamaService {
     @Value("classpath:/templates/rag-promt-meta-data.st")
     private Resource ragPromptTemplate;
 
+    @Value("classpath:/templates/system-message.st")
+    private Resource systemMessageTemplate;
+
     @Override
     public Answer getAnswer(Question question) {
+        PromptTemplate systemMessagePromptTemplate = new PromptTemplate(systemMessageTemplate);
+        Message systemMessage = systemMessagePromptTemplate.createMessage();
+
         List<Document> documents = vectorStore.similaritySearch(
-                SearchRequest.builder()
-                        .query(question.question())
-                        .topK(6)
-                        .build()
+                SearchRequest.builder().query(question.question()).topK(5).build()
         );
         List<String> contentList = documents.stream().map(Document::getContent).toList();
-        System.out.println(contentList);
-
-
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
-//        PromptTemplate promptTemplate = new PromptTemplate(question.question());
-        Prompt prompt = promptTemplate.create(Map.of(
+        Message userMessage = promptTemplate.createMessage(Map.of(
                 "input", question.question(),
                 "documents",  String.join("\n", contentList)));
-//        Prompt prompt = promptTemplate.create();
 
-        System.out.println("prompt " + prompt);
-        ChatResponse chatResponse = chatModel.call(prompt);
+
+//        List<Document> documents = vectorStore.similaritySearch(
+//                SearchRequest.builder()
+//                        .query(question.question())
+//                        .topK(6)
+//                        .build()
+//        );
+//        List<String> contentList = documents.stream().map(Document::getContent).toList();
+//        System.out.println(contentList);
+//        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+////        PromptTemplate promptTemplate = new PromptTemplate(question.question());
+//        Prompt prompt = promptTemplate.create(Map.of(
+//                "input", question.question(),
+//                "documents",  String.join("\n", contentList)));
+////        Prompt prompt = promptTemplate.create();
+//        ChatResponse chatResponse = chatModel.call(prompt);
+
+        System.out.println("userMessage " + userMessage);
+        ChatResponse chatResponse = chatModel.call(new Prompt(List.of(systemMessage, userMessage)));
         return new Answer(chatResponse.getResult().getOutput().getContent());
     }
 
